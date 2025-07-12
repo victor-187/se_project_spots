af0ad2cbe771b5cf7b406d2a30e91fb2b1e1e291
victor reyes <imperiobulldog915@gmail.com>
2025-07-11 18:22:10 -0600

    project 4 final stage

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,29 @@
 const editProfilebtn = document.querySelector(".profile__edit-btn");
 const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProfileClosebtn = editProfileModal.querySelector(".modal__close-btn");
+const editProfileForm = editProfileModal.querySelector(".modal__form");
+const editProfileNameInput = editProfileModal.querySelector(
+  "#profile-name-input"
+);
+const editProfileDescriptionInput = editProfileModal.querySelector(
+  "#profile-description-input"
+);
 
 const newPostbtn = document.querySelector(".profile__add-btn");
 const newPostModal = document.querySelector("#new-post-modal");
 const newPostClosebtn = newPostModal.querySelector(".modal__close-btn");
 
+const profileNameEl = document.querySelector(".profile__name");
+const profileDescriptionEl = document.querySelector(".profile__description");
+
+const submitProfileForm = newPostModal.querySelector(".modal__form");
+const captionInput = newPostModal.querySelector("#card-caption-input");
+const linkInput = newPostModal.querySelector("#card-image-input");
+
 editProfilebtn.addEventListener("click", function () {
+  editProfileNameInput.value = profileNameEl.textContent;
+  editProfileDescriptionInput.value = profileDescriptionEl.textContent;
+
   editProfileModal.classList.add("modal__is-opened");
 });
 
@@ -21,3 +38,22 @@ newPostbtn.addEventListener("click", function () {
 newPostClosebtn.addEventListener("click", function () {
   newPostModal.classList.remove("modal__is-opened");
 });
+
+function handleEditProfileSubmit(evt) {
+  evt.preventDefault();
+  profileNameEl.textContent = editProfileNameInput.value;
+  profileDescriptionEl.textContent = editProfileDescriptionInput.value;
+  editProfileModal.classList.remove("modal__is-opened");
+}
+
+editProfileForm.addEventListener("submit", handleEditProfileSubmit);
+
+function handleAddProfileSubmit(evt) {
+  evt.preventDefault();
+
+  newPostModal.classList.remove("modal__is-opened");
+  console.log(linkInput.value);
+  console.log(captionInput.value);
+}
+
+submitProfileForm.addEventListener("submit", handleAddProfileSubmit);
